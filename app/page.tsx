87399df5ae0b95b4322b5b54ba2d42013e788569
FatngatirBilek <fathirbimashabri@gmail.com>
2025-03-11 23:07:25 +0700

    add typewriter text

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Stat from "@/components/Stat";
 import Services from "@/components/Services";
 import Resume from "@/components/Resume";
 import Work from "@/components/Work";
+import Type from "@/components/Type";
 
 export default function Home() {
   return (
@@ -14,7 +15,9 @@ export default function Home() {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Developer</span>
+            <span className="text-xl">
+              <Type />
+            </span>
             <h1 className="h1">
               Hello, I'm <br /> <span className="text-accent">Fathir</span>
             </h1>
