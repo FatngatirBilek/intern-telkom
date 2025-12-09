f14e2d849b78e15d67bc5f93f3a9e4986f94cc84
FatngatirBilek <fathirbimashabri@gmail.com>
2025-12-09 08:37:19 +0700

    idk card

--- app/page.tsx
+++ app/page.tsx
@@ -11,66 +11,22 @@ import Spotify from "@/components/Spotify";
 import Card from "@/components/Card";
 import TeamSection from "@/components/TeamSection";
 import PriceCard from "@/components/PriceCard";
+import { Hero1 } from "@/components/hero1";
+import heroimg from "@/public/images/indihome-hero.jpg";
 export default function Home() {
   return (
     <section className="h-full">
       <div id="hero" className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          {/* text */}
-          <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">
-              <Type />
-            </span>
-            <h1 className="h1">
-              Hello, We Are <br /> <span className="text-accent">Netvara</span>
-            </h1>
-            <p className="max-w[500px] mb-9 text-text-primary">
-              Kami adalah sebuah tim web programming yang terdiri dari
-              individu-individu yang bersemangat dan berdedikasi. Kami bersatu
-              dengan satu tujuan: menciptakan solusi digital yang inovatif dan
-              efektif.
-            </p>
-            {/* Socials */}
-            <div className="flex flex-col xl:flex-row items-center gap-8">
-              {/*<a
-                href="/cv/cv.pdf"
-                download
-                target="_blank"
-                rel="noopener noreferrer"
-                className="contents"
-              >
-                <Button
-                  variant={"outline"}
-                  size={"lg"}
-                  className="uppercase flex items-center gap-2"
-                >
-                  <FiDownload className="text-xl" />
-                  Download CV
-                </Button>
-              </a>*/}
-              <div className="mb-8 xl:mb-0 ">
-                <Socials
-                  containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accnt rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
-                />
-              </div>
-            </div>
-          </div>
-          {/* photo */}
-          <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo />
-          </div>
-        </div>
+        <Hero1
+          heading="Internet Cepat & Stabil untuk Keluarga Anda"
+          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          image={{
+            src: heroimg.src,
+            alt: "Internet Cepat & Stabil untuk Keluarga Anda",
+          }}
+        />
       </div>
-      <div id="stat">
-        <Stat />
-      </div>
-      {/* <div id="services">
-        <Services />
-      </div> */}
-      <div id="resume">
-        <Resume />
-      </div>
+
       <div id="work">
         <Work />
       </div>
