6232a54868cb394e121cd0a4ccde265632269c3e
FatngatirBilek <fathirbimashabri@gmail.com>
2025-12-22 17:39:18 +0700

    prisma update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
   };
 
   outputs = {nixpkgs, ...}: let
@@ -12,10 +12,9 @@
         prisma
       ];
       shellHook = ''
+
         export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
         export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-        export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-        export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
         export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
       '';
     };
